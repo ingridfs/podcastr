feb52aad4c2605315044715c4ee79b9abf6b7027
ingridfs <ingrid.fernandes.santos@hotmail.com>
2021-04-21 21:26:13 -0300

    Home style and connection with api

---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
+
+type Episode = {
+    id: string;     
+    title: string; 
+    members: string;  
+    published_at: string;
+    thumbnail: string;
+    description: string;
+    file: {
+      url: string;
+      type: string;
+      duration: string;
+    }
+  
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <div>{JSON.stringify(props.episodes)}</div>
+    </div>
+  )
+}
+
+const getStaticProps: GetStaticProps = async () => {
+
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  })
+
+  return {
+    props: { 
+      episodes: data 
+    },
+    revalidate: 60 * 60 * 8,
+  }
+}
